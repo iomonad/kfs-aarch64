eff786a84a9069139f13fd18ff39dd9946bde19c
iomonad <iomonad@riseup.net>
2022-04-28 22:59:49 +0200

    basic implementation

--- Makefile
+++ Makefile
@@ -2,5 +2,9 @@
 # (c) iomonad - <iomonad@riseup.net>
 #
 
-CC = aarch64-linux-gnu-gcc
-LD = aarch64-linux-gnu-ld.gold
+CC  = aarch64-linux-gnu-gcc
+ASM = aarch64-elf-as -c
+LD  = aarch64-linux-gnu-ld.gold
+
+all:
+	  ${CC} boot.s -o boot.o
---
+++ boot.s
@@ -0,0 +1,42 @@
+// filename: boot.s
+// comment: aarch64 mode
+// nasm arguments: aarch64-elf-as -c boot.S -o boot.o
+
+.section ".text.boot"
+.globl _start
+
+// Memory entry point for the kernel
+// Registers:
+//   x0 -> 32 bit pointer to DTB in memory (primary core only) / 0 (secondary cores)
+//   x1 -> 0
+//   x2 -> 0
+//   x3 -> 0
+//   x4 -> 32 bit kernel entry point, _start location
+
+.org 0x80000
+_start:
+  // stack setup
+  ldr   x5, =_start
+  mov   sp, x5
+  // bss cleanup
+  ldr   x5, =__bss_start
+  ldr   w6, =__bss_size
+
+// https://github.com/raspberrypi/tools/blob/master/armstubs/armstub.S#L35
+3:
+  cbz   w6, 4f
+  str   xzr, [x5], #8
+  sub   w6, w6, #1
+  cbnz  w6, 3b
+
+// jump to C kernel code and should
+// not return. For failsafe,
+// halt this core too
+4:
+  bl kernel_main
+  b 1b
+
+// halt
+halt:
+  wfe
+  b     halt
